0d6071c1f52125b371e9b13b21883a5e9b1ac1f6
N-sangwa <nadiinesangwa@gmail.com>
2022-02-17 21:29:39 +0300

    Here is my Assignment.

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var name= "Name: Nadine Sangwa"
+    var age= "Age: 22"
+    var phoneNumber= "Phone: +250787537912"
+    var weight= "weight: 70kg"
+    var nationality : Boolean = false
+
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(nationality)
+
+
+}
